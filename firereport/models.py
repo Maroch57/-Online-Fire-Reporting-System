91ffa4e64997f55d52ebb2293402469a21d496e3
mesooneet <mesooneet@gmail.com>
2022-08-03 20:40:07 +0545

    project start

---
+++ firereport/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+class Team(models.Model):
+    teamName = models.CharField(max_length=100,null=True)
+    teamLeaderName = models.CharField(max_length=100,null=True)
+    teamMobno = models.CharField(max_length=20,null=True)
+    teamMembers = models.CharField(max_length=200,null=True)
+    postingDate = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.teamName
+
+class Firereport(models.Model):
+    fullName = models.CharField(max_length=100,null=True)
+    mobileNumber = models.CharField(max_length=20,null=True)
+    location = models.CharField(max_length=200,null=True)
+    message = models.CharField(max_length=200,null=True)
+    assignTo = models.ForeignKey(Team,on_delete=models.CASCADE,null=True)
+    status = models.CharField(max_length=200,null=True)
+    postingDate = models.DateTimeField(auto_now_add=True)
+    assignedTime = models.CharField(max_length=200,null=True)
+    updationDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.fullName    
+
+class ReportHistory(models.Model):
+    firereport = models.ForeignKey(Firereport,on_delete=models.CASCADE,null=True)
+    status = models.CharField(max_length=200,null=True)
+    remark = models.CharField(max_length=200,null=True)
+    postingDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status    
---
+++ firereport_system/urls.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from firereport import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index,name='index'),
+    path('reporting/', views.reporting,name='reporting'),
+    path('viewStatus/', views.viewStatus,name='viewStatus'),
+    path('admin_login/', views.admin_login,name='admin_login'),
+    path('dashboard/', views.dashboard,name='dashboard'),
+    path('logout/', views.Logout,name='logout'),
+    path('addTeam/', views.addTeam,name='addTeam'),
+    path('manageTeam/', views.manageTeam,name='manageTeam'),
+    path('editTeam/<int:pid>/', views.editTeam,name='editTeam'),
+    path('deleteTeam/<int:pid>/', views.deleteTeam,name='deleteTeam'),
+    path('changePassword/', views.changePassword,name='changePassword'),
+    path('search/', views.search,name='search'),
+    path('allRequest/', views.allRequest,name='allRequest'),
+    path('newRequest/', views.newRequest,name='newRequest'),
+    path('assignRequest/', views.assignRequest,name='assignRequest'),
+    path('teamontheway/', views.teamontheway,name='teamontheway'),
+    path('workinprogress/', views.workinprogress,name='workinprogress'),
+    path('completeRequest/', views.completeRequest,name='completeRequest'),
+]+static(settings.MEDIA_URL,documentsroot=settings.MEDIA_ROOT)
